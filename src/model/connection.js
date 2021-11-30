92d9287155adc463f854b924769778c086a83bf6
igor <igorsife80@gmail.com>
2021-11-30 10:04:24 -0300

    cria a connection

---
+++ src/model/connection.js
@@ -0,0 +1,18 @@
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+
+// const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager'; 
+const MONGO_DB_URL = `mongodb://${process.env.HOST || 'mongodb'}:27017/Cookmaster`;
+const DB_NAME = 'Cookmaster';
+
+const connection = () => MongoClient
+  .connect(MONGO_DB_URL, {
+    useUnifiedTopology: true,
+  })
+  .then((conn) => conn.db(DB_NAME))
+  .catch((err) => {
+    console.error(err);
+    process.exit();
+  });
+
+module.exports = connection;
