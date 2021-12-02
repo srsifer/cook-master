df96122a56e01711ae16d721b6a94ba529881ea0
igor <igorsife80@gmail.com>
2021-12-02 12:05:08 -0300

    adicion query para inserir um admin no db

--- seed.js
+++ seed.js
@@ -1,5 +1,1 @@
-// colocar query do MongoDB
-// query que n faço idei de como criar separado 
-
-/*db.users.insertOne({ name: \'admin\', email: \'root@email.com\', password: \'admin\', role: \'admin\
-module.exports = */
+db.users.insertOne({ name: 'admin', email: 'root@email.com', password: 'admin', role: 'admin' });
